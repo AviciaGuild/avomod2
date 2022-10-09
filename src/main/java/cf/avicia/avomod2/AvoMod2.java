b32486fa36589c669f1ff1a3c5d61c064188ff27
albarv340 <54350887+albarv340@users.noreply.github.com>
2022-10-09 21:41:07 +0200

    initial commit

---
+++ src/main/java/cf/avicia/avomod2/AvoMod2.java
@@ -0,0 +1,10 @@
+package cf.avicia.avomod2;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AvoMod2 implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/cf/avicia/avomod2/client/AvoMod2Client.java
@@ -0,0 +1,13 @@
+package cf.avicia.avomod2.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class AvoMod2Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
