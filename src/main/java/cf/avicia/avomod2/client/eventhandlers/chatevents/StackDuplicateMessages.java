b26139b56e0264ff1b1582f69c349c7f4975577b
albarv340 <54350887+albarv340@users.noreply.github.com>
2023-07-23 01:12:39 +0200

    Adjust StackDuplicateMessages to use accesswideners and add a space if there is no trailing space in the message

--- src/main/java/cf/avicia/avomod2/client/eventhandlers/chatevents/StackDuplicateMessages.java
+++ src/main/java/cf/avicia/avomod2/client/eventhandlers/chatevents/StackDuplicateMessages.java
@@ -4,11 +4,9 @@ import cf.avicia.avomod2.client.configs.ConfigsHandler;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.gui.hud.ChatHudLine;
-import net.minecraft.text.OrderedText;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 
-import java.lang.reflect.Field;
 import java.util.List;
 
 public class StackDuplicateMessages {
@@ -22,53 +20,29 @@ public class StackDuplicateMessages {
 
         try {
             ChatHud chatHud = MinecraftClient.getInstance().inGameHud.getChatHud();
-            Field messagesField = null;
-            Field visibleMessagesField = null;
-            for (Field declaredField : chatHud.getClass().getSuperclass().getDeclaredFields()) {
-                // If chat overlay such as wynntils is used, check the parent
-                if (declaredField.getName().equals("visibleMessages") || declaredField.getName().equals("field_2064")) {
-                    visibleMessagesField = declaredField;
-                }
-                if (declaredField.getName().equals("messages") || declaredField.getName().equals("field_2061")) {
-                    messagesField = declaredField;
-                }
-            }
-            for (Field declaredField : chatHud.getClass().getDeclaredFields()) {
-                // Also check regular chatHud
-                if (declaredField.getName().equals("visibleMessages") || declaredField.getName().equals("field_2064")) {
-                    visibleMessagesField = declaredField;
-                }
-                if (declaredField.getName().equals("messages") || declaredField.getName().equals("field_2061")) {
-                    messagesField = declaredField;
-                }
-            }
-            if (visibleMessagesField != null && messagesField != null) {
-                visibleMessagesField.setAccessible(true);
-                messagesField.setAccessible(true);
-                List<ChatHudLine.Visible> visibleMessages = (List<ChatHudLine.Visible>) visibleMessagesField.get(chatHud);
-                List<ChatHudLine> messages = (List<ChatHudLine>) messagesField.get(chatHud);
-                if (lastMessage != null && lastMessage.equals(message)) {
-                    duplicateCount++;
-                    // Visible messages are split per line, not per message, so remove all lines that were created at
-                    // the same time as the last message to remove the visible message
-                    if (visibleMessages.size() > 0) {
-                        while (!visibleMessages.get(0).equals(lastVisibleMessage)) {
-                            visibleMessages.remove(0);
-                            if (visibleMessages.size() == 0) break;
-                        }
-                        messages.remove(0);
-                    }
-
-                    // If the previous message is successfully deleted add the duplicate count to the new message
-                    // This makes it appear as the previous message gets a number added to it, since they have the same text
-                    message.getSiblings().add(Text.of(String.format("§7(%s)", duplicateCount)));
-                } else {
-                    duplicateCount = 1;
-                    lastMessage = message;
-                }
+            List<ChatHudLine.Visible> visibleMessages = chatHud.visibleMessages;
+            List<ChatHudLine> messages = chatHud.messages;
+            if (lastMessage != null && lastMessage.equals(message)) {
+                duplicateCount++;
+                // Visible messages are split per line, not per message, so remove all lines that were created at
+                // the same time as the last message to remove the visible message
                 if (visibleMessages.size() > 0) {
-                    lastVisibleMessage = visibleMessages.get(0);
+                    while (!visibleMessages.get(0).equals(lastVisibleMessage)) {
+                        visibleMessages.remove(0);
+                        if (visibleMessages.size() == 0) break;
+                    }
+                    messages.remove(0);
                 }
+
+                // If the previous message is successfully deleted add the duplicate count to the new message
+                // This makes it appear as the previous message gets a number added to it, since they have the same text
+                message.getSiblings().add(Text.of((message.getString().endsWith(" ") ? "" : " ") + String.format("§7(%s)", duplicateCount)));
+            } else {
+                duplicateCount = 1;
+                lastMessage = message;
+            }
+            if (visibleMessages.size() > 0) {
+                lastVisibleMessage = visibleMessages.get(0);
             }
         } catch (Exception e) {
             e.printStackTrace();
